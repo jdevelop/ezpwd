0957f31257241427888ab729baa81f529d839c5b
Eugene <jdevelop@users.noreply.github.com>
2019-12-08 21:30:22 -0500

    Fixed table layout.

--- ezpwd_gui/package.go
+++ ezpwd_gui/package.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
@@ -15,17 +20,21 @@ func modal(p tview.Primitive, width, height int) tview.Primitive {
 		AddItem(nil, 0, 1, false)
 }
 
-func (e *devEzpwd) showMessage(msg string, previousScreen string) {
+func (e *devEzpwd) showMessage(title, msg, previousScreen string, styleUpd ...func(*tview.TextView)) {
 	e.app.QueueUpdateDraw(func() {
 		text := tview.NewTextView().
 			SetText(msg).
-			SetWrap(true)
+			SetWrap(true).
+			SetTextAlign(tview.AlignCenter)
 		text.
-			SetTitle(" Error ").
+			SetTitle(title).
 			SetTitleColor(tcell.ColorRed).
 			SetBorder(true).
 			SetBorderColor(tcell.ColorRed).
 			SetBorderPadding(1, 1, 1, 1)
+		for _, f := range styleUpd {
+			f(text)
+		}
 		flex := tview.NewFlex().
 			SetDirection(tview.FlexRow).
 			AddItem(nil, 0, 2, false).
@@ -63,4 +72,21 @@ func (e *devEzpwd) confirm(msg, from string, ok func()) {
 	})
 	e.pages.AddPage(screenConfirm, modal(form, len(msg)+4, 5), true, true)
 	e.app.SetFocus(form)
+}
+
+const layout = "020106"
+
+func backup(file string) error {
+	current := time.Now()
+	newName := fmt.Sprintf("%s.%s-%d", file, current.Format(layout), current.Unix())
+	src, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(newName)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(f, src)
+	return err
 }
